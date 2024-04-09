32f2b722b39498acada29c5095e2b318877e748d
paquisha <paquishaxd@gmail.com>
2024-04-08 22:39:15 -0500

    se agrega back en java

---
+++ clinical/src/main/java/com/amaru/clinical/clinical/AmaruClinicalApplication.java
@@ -0,0 +1,13 @@
+package com.amaru.clinical.clinical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaruClinicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaruClinicalApplication.class, args);
+	}
+
+}
